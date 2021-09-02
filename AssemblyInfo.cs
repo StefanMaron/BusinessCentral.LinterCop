4515edd5b626469f6c2d6136cad61fd9bce9d038
StefanMaron <stefanmaron@outlook.de>
2021-09-02 09:57:10 +0200

    first commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+//[assembly: Extension]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyFileVersion("7.4.7.37768")]
+[assembly: AssemblyInformationalVersion("7.4.7.37768")]
+[assembly: AssemblyVersion("7.4.7.37768")]
---
+++ LinterCopAnalyzers.Generated.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
+using System;
+
+namespace BusinessCentral.LinterCop
+{
+  public static class DiagnosticDescriptors
+  {
+        public static readonly DiagnosticDescriptor Rule0001FlowFieldsShouldNotBeEditable = new DiagnosticDescriptor(LinterCopAnalyzers.AnalyzerPrefix + "0001", (LocalizableString)new LocalizableResourceString("Rule0001FlowFieldsShouldNotBeEditable", LinterCopAnalyzers.ResourceManager, typeof(LinterCopAnalyzers)), (LocalizableString)new LocalizableResourceString("Rule0001FlowFieldsShouldNotBeEditableFormat", LinterCopAnalyzers.ResourceManager, typeof(LinterCopAnalyzers)), "Design", DiagnosticSeverity.Warning, true, (LocalizableString)new LocalizableResourceString("Rule0001FlowFieldsShouldNotBeEditableDescription", LinterCopAnalyzers.ResourceManager, typeof(LinterCopAnalyzers)), (string)null, Array.Empty<string>());
+    }
+}
