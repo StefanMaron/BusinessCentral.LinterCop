cf0c43919daed19eb433acbe0b6287c22bae19c2
Rob van Bekkum <rob_van_bekkum@hotmail.com>
2021-10-13 10:11:14 +0200

    Pass supported types to RegisterSymbolAction

--- Design/Rule0011AccessPropertyShouldAlwaysBeSet.cs
+++ Design/Rule0011AccessPropertyShouldAlwaysBeSet.cs
@@ -15,39 +15,16 @@ namespace BusinessCentral.LinterCop.Design
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create<DiagnosticDescriptor>(DiagnosticDescriptors.Rule0011AccessPropertyShouldAlwaysBeSet);
 
         public override void Initialize(AnalysisContext context)
-            => context.RegisterSymbolAction(new Action<SymbolAnalysisContext>(this.CheckForMissingAccessProperty), SymbolKindExtensions.AllApplicationObjectSymbolKinds);
+            => context.RegisterSymbolAction(new Action<SymbolAnalysisContext>(this.CheckForMissingAccessProperty), SymbolKind.Codeunit, SymbolKind.Enum, SymbolKind.Interface, SymbolKind.Page, SymbolKind.PermissionSet, SymbolKind.Query, SymbolKind.Table, SymbolKind.XmlPort);
 
         private void CheckForMissingAccessProperty(SymbolAnalysisContext context)
         {
             IApplicationObjectTypeSymbol applicationObject = (IApplicationObjectTypeSymbol)context.Symbol;
-            
-            if (!new SymbolKind[] { SymbolKind.Codeunit, SymbolKind.Enum, SymbolKind.Interface, SymbolKind.Page, SymbolKind.PermissionSet, SymbolKind.Query, SymbolKind.Table, SymbolKind.XmlPort }.Contains(applicationObject.Kind))
-            {
-                return;
-            }
-
-            if (!IsSymbolAccessible(applicationObject))
-            {
-                return;
-            }
 
             if (applicationObject.GetProperty(PropertyKind.Access) == null)
             {
                 context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0011AccessPropertyShouldAlwaysBeSet, applicationObject.GetLocation()));
             }
         }
-
-        private static bool IsSymbolAccessible(ISymbol symbol) {
-            try {
-                GetDeclaration(symbol);
-                return true;
-            } catch(Exception) {
-                return false;
-            }
-        }
-
-        private static string GetDeclaration(ISymbol symbol)
-            => symbol.Location.SourceTree.GetText(CancellationToken.None).GetSubText(symbol.DeclaringSyntaxReference.Span).ToString();
     }
-    
 }
