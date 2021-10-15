77578d677c19859c119537cd13fa028e02634959
StefanMaron <stefanmaron@outlook.de>
2021-10-15 08:00:04 +0200

    Added Rule0012

---
+++ Design/Rule0012DoNotUseObjectIdInSystemFunctions.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Dynamics.Nav.CodeAnalysis;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using BusinessCentral.LinterCop.Helpers;
+using System.Collections.Generic;
+
+namespace BusinessCentral.LinterCop.Design
+{
+    [DiagnosticAnalyzer]
+    public class Rule0012DoNotUseObjectIdInSystemFunctions : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create<DiagnosticDescriptor>(DiagnosticDescriptors.Rule0012DoNotUseObjectIdInSystemFunctions);
+
+        public override void Initialize(AnalysisContext context)
+            => context.RegisterOperationAction(new Action<OperationAnalysisContext>(this.CheckForObjectIdsInFunctionInvocations), OperationKind.InvocationExpression);
+
+        private void CheckForObjectIdsInFunctionInvocations(OperationAnalysisContext context)
+        {
+            IInvocationExpression operation = (IInvocationExpression)context.Operation;
+            RelevantFuntion CurrentFunction = null;
+            try
+            {
+                CurrentFunction = FunctionCallsWithIDParamaters.RelevantFunctions.First(o => (o.ObjectType.ToString().ToUpper() == operation.TargetMethod.ContainingSymbol.Name.ToUpper() && o.FunctionName == operation.TargetMethod.Name));
+            }
+            catch (System.InvalidOperationException)
+            { }
+
+            SyntaxKind[] AllowedParameterKinds = { SyntaxKind.MemberAccessExpression, SyntaxKind.IdentifierName };
+            
+            if (CurrentFunction != null && operation.TargetMethod.Parameters.Length != 0 && !AllowedParameterKinds.Contains(operation.Arguments[0].Syntax.Kind) && (operation.Arguments[0].Syntax.ToString() != "0" || !CurrentFunction.ZeroIDAllowed))                
+                {
+                    if (operation.TargetMethod.Parameters[0].ParameterType.NavTypeKind == NavTypeKind.Integer)
+                    {
+                        int tempint = 0;
+                        if (int.TryParse(operation.Arguments[0].Syntax.ToString(), out tempint))
+                            context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0012DoNotUseObjectIdInSystemFunctions, context.Operation.Syntax.GetLocation(), new object[] { CurrentFunction.CorrectAccessSymbol, "" }));
+                        else
+                            if (!operation.Arguments[0].Syntax.ToString().ToUpper().StartsWith(CurrentFunction.CorrectAccessSymbol.ToUpper()))
+                            context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0012DoNotUseObjectIdInSystemFunctions, context.Operation.Syntax.GetLocation(), new object[] { CurrentFunction.CorrectAccessSymbol, "" }));
+                    }
+                }
+            
+        }
+    }
+
+    class RelevantFuntion
+    {
+        public NavTypeKind ObjectType;
+        public string FunctionName;
+        public string CorrectAccessSymbol;
+        public bool ZeroIDAllowed;
+
+        public RelevantFuntion(NavTypeKind ObjType, string FnctName,string AccessSymbol,bool ZeroID)
+        {
+            ObjectType = ObjType;
+            FunctionName = FnctName;
+            CorrectAccessSymbol = AccessSymbol;
+            ZeroIDAllowed = ZeroID;
+        }
+    }
+
+    class FunctionCallsWithIDParamaters
+    {
+        static public List<RelevantFuntion> RelevantFunctions = new List<RelevantFuntion> {
+            new RelevantFuntion(NavTypeKind.Codeunit,"Run","Codeunit",false),
+            new RelevantFuntion(NavTypeKind.Report,"Run","Report",false),
+            new RelevantFuntion(NavTypeKind.Report,"Execute","Report",false),
+            new RelevantFuntion(NavTypeKind.Report,"Print","Report",false),
+            new RelevantFuntion(NavTypeKind.Report,"RdlcLayout","Report",false),
+            new RelevantFuntion(NavTypeKind.Report,"RunModal","Report",false),
+            new RelevantFuntion(NavTypeKind.Report,"RunRequestPage","Report",false),
+            new RelevantFuntion(NavTypeKind.Report,"SaveAs","Report",false),
+            new RelevantFuntion(NavTypeKind.Report,"SaveAsExcel","Report",false),
+            new RelevantFuntion(NavTypeKind.Report,"SaveAsHtml","Report",false),
+            new RelevantFuntion(NavTypeKind.Report,"SaveAsPdf","Report",false),
+            new RelevantFuntion(NavTypeKind.Report,"SaveAsWord","Report",false),
+            new RelevantFuntion(NavTypeKind.Report,"SaveAsXml","Report",false),
+            new RelevantFuntion(NavTypeKind.XmlPort,"Run","XMLPort",false),
+            new RelevantFuntion(NavTypeKind.XmlPort,"Export","XMLPort",false),
+            new RelevantFuntion(NavTypeKind.XmlPort,"Import","XMLPort",false),
+            new RelevantFuntion(NavTypeKind.Page,"Run","Page",true),
+            new RelevantFuntion(NavTypeKind.Page,"RunModal","Page",true),
+            new RelevantFuntion(NavTypeKind.RecordRef,"Open","Database",false),
+            new RelevantFuntion(NavTypeKind.Query,"SaveAsCsv","Query",false),
+            new RelevantFuntion(NavTypeKind.Query,"SaveAsXml","Query",false)
+        };
+    }
+}
--- README.md
+++ README.md
@@ -44,6 +44,7 @@ Further note that you should have BcContainerHelper version 2.0.16 (or newer) in
 |LC0009|Show info message about code metrics for each function or trigger|Disabled|
 |LC0010|Show warning about code metrics for each function or trigger if either cyclomatic complexity is 8 or greater or maintainability index 20 or lower|Warning|
 |LC0011|Every object needs to specify a value for the `Access` property. Either `true` or `false`|Disabled|
+|LC0012|Using hardcoded IDs in functions like `Codeunit.Run()` is not allow (Preview)|Warning|
 
 ## Configuration
 
