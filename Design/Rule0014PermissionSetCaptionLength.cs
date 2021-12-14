bdfb8ecd9bae7076b630c3de2684cf8307740bc1
Rob van Bekkum <rob_van_bekkum@hotmail.com>
2021-12-14 20:52:17 +0100

    LC0014 Permission Set Caption Length

---
+++ Design/Rule0014PermissionSetCaptionLength.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Dynamics.Nav.CodeAnalysis;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Immutable;
+
+namespace BusinessCentral.LinterCop.Design
+{
+    [DiagnosticAnalyzer]
+    public class Rule0014PermissionSetCaptionLength : DiagnosticAnalyzer
+    {
+        private const int MAXCAPTIONLENGTH = 30;
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create<DiagnosticDescriptor>(DiagnosticDescriptors.Rule0014PermissionSetCaptionLength);
+
+        public override void Initialize(AnalysisContext context)
+            => context.RegisterSymbolAction(new Action<SymbolAnalysisContext>(this.CheckPermissionSetNameAndCaptionLength), SymbolKind.PermissionSet);
+
+        private void CheckPermissionSetNameAndCaptionLength(SymbolAnalysisContext context)
+        {
+            IPropertySymbol captionProperty = context.Symbol.GetProperty(PropertyKind.Caption);
+            if (captionProperty == null)
+            {
+                return;
+            }
+            if (captionProperty?.ValueText.Length > MAXCAPTIONLENGTH)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0014PermissionSetCaptionLength, captionProperty.GetLocation(), new object[] { MAXCAPTIONLENGTH }));
+            }
+        }
+    }
+}
