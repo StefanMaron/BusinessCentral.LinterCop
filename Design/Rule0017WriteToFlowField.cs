36d351eb993c48438ca1693a8aee1153430fbec3
StefanMaron <stefanmaron@outlook.de>
2022-02-23 12:09:27 +0100

    Added Rule LC0017 #142

---
+++ Design/Rule0017WriteToFlowField.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Dynamics.Nav.CodeAnalysis;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Semantics;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Syntax;
+using System;
+using System.Collections.Immutable;
+
+namespace BusinessCentral.LinterCop.Design
+{
+    [DiagnosticAnalyzer]
+    class Rule0017WriteToFlowField : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create<DiagnosticDescriptor>(DiagnosticDescriptors.Rule0017WriteToFlowField);
+
+        public override void Initialize(AnalysisContext context)
+            => context.RegisterOperationAction(new Action<OperationAnalysisContext>(this.CheckForWriteToFlowField),
+                OperationKind.AssignmentStatement,
+                OperationKind.InvocationExpression
+            );
+
+        private void CheckForWriteToFlowField(OperationAnalysisContext context)
+        {
+            if (context.ContainingSymbol.IsObsoletePending || context.ContainingSymbol.IsObsoleteRemoved) return;
+            if (context.ContainingSymbol.GetContainingObjectTypeSymbol().IsObsoletePending || context.ContainingSymbol.GetContainingObjectTypeSymbol().IsObsoleteRemoved) return;
+
+            if (context.Operation.Kind == OperationKind.InvocationExpression)
+            {
+                IInvocationExpression operation = (IInvocationExpression)context.Operation;
+                if (operation.TargetMethod.Name == "Validate")
+                {
+                    var FieldClass = ((IFieldAccess)((IConversionExpression)operation.Arguments[0].Value).Operand).FieldSymbol.FieldClass;
+                    if (FieldClass == FieldClassKind.FlowField)
+                        if (!HasExplainingComment(context.Operation))
+                            context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0017WriteToFlowField, context.Operation.Syntax.GetLocation()));
+                }
+            }
+            else
+            {
+                IAssignmentStatement operation = (IAssignmentStatement)context.Operation;
+                if (operation.Target.Kind == OperationKind.FieldAccess)
+                {
+                    var FieldClass = ((IFieldAccess)operation.Target).FieldSymbol.FieldClass;
+                    if (FieldClass == FieldClassKind.FlowField)
+                        if (!HasExplainingComment(context.Operation))
+                            context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0017WriteToFlowField, context.Operation.Syntax.GetLocation()));
+                }
+            }
+        }
+
+        private bool HasExplainingComment(IOperation operation)
+        {
+            foreach (SyntaxTrivia trivia in operation.Syntax.Parent.GetLeadingTrivia())
+            {
+                if (trivia.IsKind(SyntaxKind.LineCommentTrivia))
+                {
+                    return true;
+                }
+            }
+            foreach (SyntaxTrivia trivia in operation.Syntax.Parent.GetTrailingTrivia())
+            {
+                if (trivia.IsKind(SyntaxKind.LineCommentTrivia))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
