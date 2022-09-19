5bee34f1f7c60444617043e19711b6f4f5bf6c87
StefanMaron <stefanmaron@outlook.de>
2022-09-19 14:43:54 +0200

    LC 20 Application area redundancy

---
+++ Design/Rule0020ApplicationAreaEqualsToPage.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Dynamics.Nav.CodeAnalysis;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Immutable;
+
+namespace BusinessCentral.LinterCop.Design
+{
+    [DiagnosticAnalyzer]
+    class Rule0020ApplicationAreaEqualsToPage : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create<DiagnosticDescriptor>(DiagnosticDescriptors.Rule0020ApplicationAreaEqualsToPage);
+        public override VersionCompatibility SupportedVersions { get; } = VersionCompatibility.Fall2022OrGreater;
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSymbolAction(new Action<SymbolAnalysisContext>(CheckDataClassificationRedundancy), SymbolKind.Control);
+        }
+
+        private void CheckDataClassificationRedundancy(SymbolAnalysisContext symbolAnalysisContext)
+        {
+            IControlSymbol Control = (IControlSymbol)symbolAnalysisContext.Symbol;
+            if (Control == null || Control.IsObsoleteRemoved || Control.IsObsoletePending)
+                return;
+
+            IApplicationObjectTypeSymbol applicationObject = Control.GetContainingApplicationObjectTypeSymbol();
+            if (!(applicationObject is IPageTypeSymbol) || applicationObject.IsObsoleteRemoved || applicationObject.IsObsoletePending)
+                return;
+
+            IPageTypeSymbol Page = (IPageTypeSymbol)applicationObject;
+            IPropertySymbol controlApplicationArea = Control.GetProperty(PropertyKind.ApplicationArea) as IPropertySymbol;
+            IPropertySymbol pageApplicationArea = Page.GetProperty(PropertyKind.ApplicationArea) as IPropertySymbol;
+
+            if (controlApplicationArea == null || pageApplicationArea == null)
+                return;
+
+            if (pageApplicationArea.ValueText.Contains(controlApplicationArea.ValueText))
+                symbolAnalysisContext.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0020ApplicationAreaEqualsToPage, controlApplicationArea.GetLocation()));
+        }
+
+    }
+}
