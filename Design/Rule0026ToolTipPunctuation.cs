e8193f856cf7b51e325981da9cb6d445ba3c2d5a
Tine Staric <tine@staric.net>
2023-02-04 23:00:32 +0200

    Add Rule 0026 - ToolTips should end with a dot.

---
+++ Design/Rule0026ToolTipPunctuation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Immutable;
+using Microsoft.Dynamics.Nav.CodeAnalysis;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Syntax;
+
+namespace BusinessCentral.LinterCop.Design
+{
+    [DiagnosticAnalyzer]
+    public class Rule0026ToolTipPunctuation : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create<DiagnosticDescriptor>(DiagnosticDescriptors.Rule0026ToolTipPunctuation);
+
+        public override void Initialize(AnalysisContext context) => context.RegisterSyntaxNodeAction(new Action<SyntaxNodeAnalysisContext>(this.AnalyzeToolTipPunctuation), SyntaxKind.PageField, SyntaxKind.PageAction);
+
+        private void AnalyzeToolTipPunctuation(SyntaxNodeAnalysisContext ctx)
+        {
+            if (ctx.ContainingSymbol.IsObsoletePending || ctx.ContainingSymbol.IsObsoleteRemoved) return;
+            if (ctx.ContainingSymbol.GetContainingObjectTypeSymbol().IsObsoletePending || ctx.ContainingSymbol.GetContainingObjectTypeSymbol().IsObsoleteRemoved) return;
+
+            PropertySyntax tooltipProperty = ctx.Node.GetProperty("ToolTip");
+
+            if (tooltipProperty != null)
+            {
+                string tooltipValue = tooltipProperty.Value.GetText().ToString();
+                if (!tooltipValue.EndsWith(".'"))
+                    ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0026ToolTipPunctuation, tooltipProperty.GetLocation()));
+            }
+
+        }
+    }
+}
