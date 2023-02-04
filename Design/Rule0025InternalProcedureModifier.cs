c4578f0a9096c9e79b23f4c4f9b93182be34f93b
Tine Staric <tine@staric.net>
2023-02-04 22:53:49 +0200

    Add Rule 0025 - Procedure Access Modifier must be Specified

---
+++ Design/Rule0025InternalProcedureModifier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Immutable;
+using Microsoft.Dynamics.Nav.CodeAnalysis;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Syntax;
+
+namespace BusinessCentral.LinterCop.Design
+{
+    [DiagnosticAnalyzer]
+    public class Rule0025InternalProcedureModifier : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create<DiagnosticDescriptor>(DiagnosticDescriptors.Rule0025InternalProcedureModifier);
+
+        public override void Initialize(AnalysisContext context) => context.RegisterSyntaxNodeAction(new Action<SyntaxNodeAnalysisContext>(this.AnalyzeInternalProcedures), SyntaxKind.MethodDeclaration);
+
+        private void AnalyzeInternalProcedures(SyntaxNodeAnalysisContext ctx)
+        {
+            if (ctx.ContainingSymbol.IsObsoletePending || ctx.ContainingSymbol.IsObsoleteRemoved) return;
+            if (ctx.ContainingSymbol.GetContainingObjectTypeSymbol().IsObsoletePending || ctx.ContainingSymbol.GetContainingObjectTypeSymbol().IsObsoleteRemoved) return;
+
+            MethodDeclarationSyntax syntax = ctx.Node as MethodDeclarationSyntax;
+            SyntaxNodeOrToken accessModifier = syntax.ProcedureKeyword.GetPreviousToken();
+
+            if (accessModifier.Kind != SyntaxKind.LocalKeyword && accessModifier.Kind != SyntaxKind.InternalKeyword)
+                ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0025InternalProcedureModifier, syntax.ProcedureKeyword.GetLocation()));
+
+        }
+    }
+}
