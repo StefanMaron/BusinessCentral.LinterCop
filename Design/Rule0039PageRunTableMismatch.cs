220801c3b6802d57aba5d6069c35aecafc292ae6
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2023-11-29 11:55:08 +0100

    Extend rule 0039 with SetRecord()

--- Design/Rule0039PageRunTableMismatch.cs
+++ Design/Rule0039PageRunTableMismatch.cs
@@ -3,6 +3,7 @@ using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
 using Microsoft.Dynamics.Nav.CodeAnalysis.Symbols;
 using Microsoft.Dynamics.Nav.CodeAnalysis.Syntax;
 using System.Collections.Immutable;
+using System.Reflection;
 
 namespace BusinessCentral.LinterCop.Design
 {
@@ -11,7 +12,11 @@ namespace BusinessCentral.LinterCop.Design
     {
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create<DiagnosticDescriptor>(DiagnosticDescriptors.Rule0039ArgumentDifferentTypeThenExpected);
 
-        public override void Initialize(AnalysisContext context) => context.RegisterOperationAction(new Action<OperationAnalysisContext>(this.AnalyzeRunPageArguments), OperationKind.InvocationExpression);
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterOperationAction(new Action<OperationAnalysisContext>(this.AnalyzeRunPageArguments), OperationKind.InvocationExpression);
+            context.RegisterOperationAction(new Action<OperationAnalysisContext>(this.AnalyzeSetRecordArgument), OperationKind.InvocationExpression);
+        }
 
         private void AnalyzeRunPageArguments(OperationAnalysisContext ctx)
         {
@@ -22,6 +27,7 @@ namespace BusinessCentral.LinterCop.Design
             if (operation.TargetMethod.MethodKind != MethodKind.BuiltInMethod) return;
 
             if (operation.TargetMethod.ContainingType.GetTypeSymbol().GetNavTypeKindSafe() != NavTypeKind.Page) return;
+            if (!SemanticFacts.IsSameName(operation.TargetMethod.Name, "Run") && !SemanticFacts.IsSameName(operation.TargetMethod.Name, "RunModal")) return;
             if (operation.Arguments.Count() < 2) return;
 
             if (operation.Arguments[0].Syntax.Kind != SyntaxKind.OptionAccessExpression) return;
@@ -29,14 +35,42 @@ namespace BusinessCentral.LinterCop.Design
 
             IApplicationObjectTypeSymbol applicationObjectTypeSymbol = ((IApplicationObjectAccess)operation.Arguments[0].Value).ApplicationObjectTypeSymbol;
             if (applicationObjectTypeSymbol.GetNavTypeKindSafe() != NavTypeKind.Page) return;
-            ITableTypeSymbol pageObjSourceTable = ((IPageTypeSymbol)applicationObjectTypeSymbol.GetTypeSymbol()).RelatedTable;
-            if (pageObjSourceTable == null) return;
+            ITableTypeSymbol pageSourceTable = ((IPageTypeSymbol)applicationObjectTypeSymbol.GetTypeSymbol()).RelatedTable;
+            if (pageSourceTable == null) return;
 
             IOperation operand = ((IConversionExpression)operation.Arguments[1].Value).Operand;
             ITableTypeSymbol recordArgument = ((IRecordTypeSymbol)operand.GetSymbol().GetTypeSymbol()).BaseTable;
 
-            if (recordArgument != pageObjSourceTable)
-                ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0039ArgumentDifferentTypeThenExpected, ctx.Operation.Syntax.GetLocation(), new object[] { 2, operand.GetSymbol().GetTypeSymbol().ToString(), "Record \"" + pageObjSourceTable.Name + "\"" }));
+            if (recordArgument != pageSourceTable)
+                ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0039ArgumentDifferentTypeThenExpected, ctx.Operation.Syntax.GetLocation(), new object[] { 2, operand.GetSymbol().GetTypeSymbol().ToString(), "Record \"" + pageSourceTable.Name + "\"" }));
+        }
+
+        private void AnalyzeSetRecordArgument(OperationAnalysisContext ctx)
+        {
+            if (ctx.ContainingSymbol.GetContainingObjectTypeSymbol().IsObsoletePending || ctx.ContainingSymbol.GetContainingObjectTypeSymbol().IsObsoleteRemoved) return;
+            if (ctx.ContainingSymbol.IsObsoletePending || ctx.ContainingSymbol.IsObsoleteRemoved) return;
+
+            IInvocationExpression operation = (IInvocationExpression)ctx.Operation;
+            if (operation.TargetMethod.MethodKind != MethodKind.BuiltInMethod) return;
+
+            if (operation.TargetMethod.ContainingType.GetTypeSymbol().GetNavTypeKindSafe() != NavTypeKind.Page) return;
+            if (!SemanticFacts.IsSameName(operation.TargetMethod.Name, "SetRecord")) return;
+            if (operation.Arguments.Count() != 1) return;
+
+            if (operation.Arguments[0].Syntax.Kind != SyntaxKind.IdentifierName) return;
+
+            IOperation pageReference = ctx.Operation.DescendantsAndSelf().Where(x => x.GetSymbol() != null)
+                                                        .Where(x => x.Type.GetNavTypeKindSafe() == NavTypeKind.Page)
+                                                        .First();
+            IVariableSymbol variableSymbol = (IVariableSymbol)pageReference.GetSymbol().OriginalDefinition;
+            IPageTypeSymbol pageTypeSymbol = (IPageTypeSymbol)variableSymbol.GetTypeSymbol().OriginalDefinition;
+            ITableTypeSymbol pageSourceTable = pageTypeSymbol.RelatedTable;
+
+            IOperation operand = ((IConversionExpression)operation.Arguments[0].Value).Operand;
+            ITableTypeSymbol recordArgument = ((IRecordTypeSymbol)operand.GetSymbol().GetTypeSymbol()).BaseTable;
+
+            if (recordArgument != pageSourceTable)
+                ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0039ArgumentDifferentTypeThenExpected, ctx.Operation.Syntax.GetLocation(), new object[] { 1, operand.GetSymbol().GetTypeSymbol().ToString(), "Record \"" + pageSourceTable.Name + "\"" }));
         }
     }
 }
