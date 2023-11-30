6011f78dc0118eda9af8018f445fcee0fe14a2f3
Tine Staric <tine.staric@companial.com>
2023-11-30 16:32:15 +0200

    Add Rule0040ZeroEnumValueReservedForEmpty analyzer

---
+++ Design/Rule0040ZeroEnumValueReservedForEmpty copy.cs
@@ -0,0 +1,34 @@
+using Microsoft.Dynamics.Nav.CodeAnalysis;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Syntax;
+using System.Collections.Immutable;
+
+namespace BusinessCentral.LinterCop.Design
+{
+    [DiagnosticAnalyzer]
+    public class Rule0040ZeroEnumValueReservedForEmpty : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create<DiagnosticDescriptor>(DiagnosticDescriptors.Rule0040ZeroEnumValueReservedForEmpty);
+
+        public override void Initialize(AnalysisContext context) => context.RegisterSyntaxNodeAction(new Action<SyntaxNodeAnalysisContext>(this.AnalyzeReservedEnum), SyntaxKind.EnumValue);
+
+        private void AnalyzeReservedEnum(SyntaxNodeAnalysisContext ctx)
+        {
+            if (ctx.ContainingSymbol.GetContainingObjectTypeSymbol().IsObsoletePending || ctx.ContainingSymbol.GetContainingObjectTypeSymbol().IsObsoleteRemoved) return;
+            if (ctx.ContainingSymbol.IsObsoletePending || ctx.ContainingSymbol.IsObsoleteRemoved) return;
+
+            LabelPropertyValueSyntax captionProperty = ctx.Node?.GetProperty("Caption")?.Value as LabelPropertyValueSyntax;
+            EnumValueSyntax enumValue = ctx.Node as EnumValueSyntax;
+
+            if (enumValue == null) return;
+
+            if (enumValue.Id.ValueText != "0" || ctx.ContainingSymbol.Kind != SymbolKind.Enum) return;
+
+            if (enumValue.GetNameStringValue().Trim() != "")
+                ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0040ZeroEnumValueReservedForEmpty, enumValue.Name.GetLocation()));
+            
+            if (captionProperty != null && captionProperty.Value.LabelText.Value.Value.ToString().Trim() != "")
+                ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0040ZeroEnumValueReservedForEmpty, captionProperty.GetLocation()));
+        }
+    }
+}
