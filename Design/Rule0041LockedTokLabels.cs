3fbeb0a545098f27ef550cb3b5ff9397feb0e643
Tine Staric <tine.staric@companial.com>
2023-11-30 16:32:53 +0200

    Add Rule0041LockedTokLabels diagnostic analyzer

---
+++ Design/Rule0041LockedTokLabels.cs
@@ -0,0 +1,32 @@
+using Microsoft.Dynamics.Nav.CodeAnalysis;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace BusinessCentral.LinterCop.Design
+{
+    [DiagnosticAnalyzer]
+    public class Rule0041LockedTokLabels : DiagnosticAnalyzer
+    {
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(DiagnosticDescriptors.Rule0041TokLabelsLocked, DiagnosticDescriptors.Rule0041LockedLabelsTok);
+        public override void Initialize(AnalysisContext context) => context.RegisterSymbolAction(new Action<SymbolAnalysisContext>(this.AnalyzeLockedLabel), SymbolKind.GlobalVariable, SymbolKind.LocalVariable);
+
+        private void AnalyzeLockedLabel(SymbolAnalysisContext ctx)
+        {
+            IVariableSymbol symbol = (IVariableSymbol)ctx.Symbol;
+
+            ITypeSymbol type1 = symbol.Type;
+            if (type1 == null || type1.NavTypeKind != NavTypeKind.Label)
+                return;
+
+            ILabelTypeSymbol type = symbol.Type as ILabelTypeSymbol;
+            if (type.Locked)
+                if (!type.Name.EndsWith("Tok"))
+                    ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0041LockedLabelsTok, symbol.GetLocation(), symbol.Name));
+
+            if (type.Name.EndsWith("Tok"))
+                if (!type.Locked)
+                    ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0041TokLabelsLocked, symbol.GetLocation()));
+
+        }
+    }
+}
