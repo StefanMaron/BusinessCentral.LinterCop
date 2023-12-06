189a126df9ec91641c8556751f9cc18b0ecd75fd
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2023-12-06 15:12:10 +0100

    Expand to more objects and controls

--- Design/Rule0041EmptyCaptionLocked.cs
+++ Design/Rule0041EmptyCaptionLocked.cs
@@ -1,7 +1,6 @@
 using Microsoft.Dynamics.Nav.CodeAnalysis;
 using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
 using Microsoft.Dynamics.Nav.CodeAnalysis.Syntax;
-using System;
 using System.Collections.Immutable;
 
 namespace BusinessCentral.LinterCop.Design
@@ -11,31 +10,53 @@ namespace BusinessCentral.LinterCop.Design
     {
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(DiagnosticDescriptors.Rule0041EmptyCaptionLocked);
 
-        public override void Initialize(AnalysisContext context) => context.RegisterSyntaxNodeAction(new Action<SyntaxNodeAnalysisContext>(AnalyzeCaptionProperty), SyntaxKind.EnumValue);
+        // List based on https://learn.microsoft.com/en-us/dynamics365/business-central/dev-itpro/developer/properties/devenv-caption-property
+        public override void Initialize(AnalysisContext context)
+            => context.RegisterSyntaxNodeAction(new Action<SyntaxNodeAnalysisContext>(AnalyzeCaptionProperty), new SyntaxKind[] {
+                SyntaxKind.TableObject,
+                SyntaxKind.Field, // TableField
+                SyntaxKind.PageField,
+                SyntaxKind.PageGroup,
+                SyntaxKind.PageObject,
+                SyntaxKind.RequestPage,
+                SyntaxKind.PageLabel,
+                SyntaxKind.PageGroup,
+                SyntaxKind.PagePart,
+                SyntaxKind.PageSystemPart,
+                SyntaxKind.PageAction,
+                SyntaxKind.PageActionSeparator,
+                SyntaxKind.PageActionGroup,
+                SyntaxKind.XmlPortObject,
+                SyntaxKind.ReportObject,
+                SyntaxKind.QueryObject,
+                SyntaxKind.QueryColumn,
+                SyntaxKind.QueryFilter,
+                SyntaxKind.ReportColumn,
+                SyntaxKind.EnumValue,
+                SyntaxKind.PageCustomAction,
+                SyntaxKind.PageSystemAction,
+                SyntaxKind.PageView,
+                SyntaxKind.ReportLayout,
+                SyntaxKind.ProfileObject,
+                SyntaxKind.EnumType,
+                SyntaxKind.PermissionSet,
+                SyntaxKind.TableExtensionObject,
+                SyntaxKind.PageExtensionObject
+            });
 
         private void AnalyzeCaptionProperty(SyntaxNodeAnalysisContext ctx)
         {
             if (ctx.ContainingSymbol.IsObsoletePending || ctx.ContainingSymbol.IsObsoleteRemoved) return;
             if (ctx.ContainingSymbol.GetContainingObjectTypeSymbol().IsObsoletePending || ctx.ContainingSymbol.GetContainingObjectTypeSymbol().IsObsoleteRemoved) return;
 
+            if (ctx.Node.IsKind(SyntaxKind.EnumValue) && ctx.ContainingSymbol.Kind == SymbolKind.Enum) return; // Prevent double raising the rule on EnumValue in a EnumObject
+
             LabelPropertyValueSyntax captionProperty = ctx.Node?.GetProperty("Caption")?.Value as LabelPropertyValueSyntax;
+            if (captionProperty?.Value.LabelText.GetLiteralValue() == null || captionProperty.Value.LabelText.GetLiteralValue().ToString().Trim() != "") return;
 
-            if (captionProperty == null || captionProperty.Value.LabelText.Value.Value.ToString().Trim() != "" || ctx.ContainingSymbol.Kind != SymbolKind.Enum) return;
+            if (captionProperty.Value.Properties?.Values.Where(prop => prop.Identifier.Text.ToLowerInvariant() == "locked").FirstOrDefault() != null) return;
 
-            bool labelLocked = false;
-
-            if (captionProperty.Value.Properties != null)
-                foreach (IdentifierEqualsLiteralSyntax property in captionProperty.Value.Properties.Values)
-                {
-                    if (property.Identifier.Text.ToLower() == "locked")
-                    {
-                        labelLocked = true;
-                        break;
-                    }
-                }
-
-            if (!labelLocked)
-                ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0041EmptyCaptionLocked, captionProperty.GetLocation()));
+            ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0041EmptyCaptionLocked, captionProperty.GetLocation()));
         }
     }
 }
