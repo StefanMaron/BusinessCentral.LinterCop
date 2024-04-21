bcc989dbfc617e6c81607193b6ed13824745eb03
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2024-04-21 09:15:37 +0200

    dev

--- Design/Rule0051SetFilterPossibleOverflow.cs
+++ Design/Rule0051SetFilterPossibleOverflow.cs
@@ -1,3 +1,4 @@
+#if !Legacy
 using Microsoft.Dynamics.Nav.CodeAnalysis;
 using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
 using Microsoft.Dynamics.Nav.CodeAnalysis.Symbols;
@@ -252,3 +253,4 @@ namespace BusinessCentral.LinterCop.Design
         }
     }
 }
+#endif
