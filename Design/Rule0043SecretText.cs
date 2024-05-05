4aff652b1955705447ae0e7cd184ff25087c864a
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2024-05-05 17:30:57 +0200

    Add Spring2024OrGreater

--- Design/Rule0043SecretText.cs
+++ Design/Rule0043SecretText.cs
@@ -1,3 +1,4 @@
+#if Fall2023RV1
 using System.Collections.Immutable;
 using BusinessCentral.LinterCop.AnalysisContextExtension;
 using Microsoft.Dynamics.Nav.CodeAnalysis;
@@ -28,6 +29,7 @@ namespace BusinessCentral.LinterCop.Design
 
         private void AnalyzeIsolatedStorage(OperationAnalysisContext ctx)
         {
+#if Spring2024OrGreater
             if (!VersionChecker.IsSupported(ctx.ContainingSymbol, VersionCompatibility.Spring2024OrGreater)) return;
 
             if (ctx.IsObsoletePendingOrRemoved()) return;
@@ -58,6 +60,7 @@ namespace BusinessCentral.LinterCop.Design
 
             if (!IsArgumentOfTypeSecretText(operation.Arguments[argumentIndex]))
                 ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0043SecretText, ctx.Operation.Syntax.GetLocation()));
+#endif
         }
 
         private void AnalyzeHttpObjects(OperationAnalysisContext ctx)
@@ -115,3 +118,4 @@ namespace BusinessCentral.LinterCop.Design
         }
     }
 }
+#endif
