b85b228d76b2359354bc4c1f21378b917899c6db
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2024-09-15 14:58:37 +0200

    Change criteria to ExtensionId ("filterType":4) and remove statistics property ("flags":18)

--- .github/actions/marketplace/Get-Marketplace.ps1
+++ .github/actions/marketplace/Get-Marketplace.ps1
@@ -16,7 +16,7 @@ function ConvertTo-Version {
 
 $listing = Invoke-WebRequest -Method POST -UseBasicParsing `
     -Uri https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery?api-version=3.0-preview.1 `
-    -Body '{"filters":[{"criteria":[{"filterType":8,"value":"Microsoft.VisualStudio.Code"},{"filterType":12,"value":"4096"},{"filterType":7,"value":"ms-dynamics-smb.al"}],"pageNumber":1,"pageSize":50,"sortBy":0,"sortOrder":0}],"assetTypes":[],"flags":0x192}' `
+    -Body '{"filters":[{"criteria":[{"filterType":4,"value":"fe889a8a-1498-4047-850d-eb8ea82de1d1"}],"pageNumber":1,"pageSize":50,"sortBy":0,"sortOrder":0}],"assetTypes":[],"flags":18}' `
     -ContentType application/json | ConvertFrom-Json
 
 $listingFiltered = $listing.results | Select-Object -First 1 -ExpandProperty extensions `
