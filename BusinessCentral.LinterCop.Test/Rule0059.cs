4efa6069ce60c9129f528c394e7f8f4f9e329397
Arthur van de Vondervoort <44637996+Arthurvdv@users.noreply.github.com>
2024-11-26 17:21:07 +0100

    Extend Rule0059 to where filter expression on CalcFormula property (#817)

---
+++ BusinessCentral.LinterCop.Test/Rule0059.cs
@@ -0,0 +1,35 @@
+namespace BusinessCentral.LinterCop.Test;
+
+public class Rule0059
+{
+    private string _testCaseDir = "";
+
+    [SetUp]
+    public void Setup()
+    {
+        _testCaseDir = Path.Combine(Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.FullName,
+            "TestCases", "Rule0059");
+    }
+
+    [Test]
+    [TestCase("CalcFormulaWhere")]
+    public async Task HasDiagnostic(string testCase)
+    {
+        var code = await File.ReadAllTextAsync(Path.Combine(_testCaseDir, "HasDiagnostic", $"{testCase}.al"))
+            .ConfigureAwait(false);
+
+        var fixture = RoslynFixtureFactory.Create<Rule0059SingleQuoteEscaping>();
+        fixture.HasDiagnostic(code, DiagnosticDescriptors.Rule0059SingleQuoteEscapingIssueDetected.Id);
+    }
+
+    [Test]
+    [TestCase("CalcFormulaWhere")]
+    public async Task NoDiagnostic(string testCase)
+    {
+        var code = await File.ReadAllTextAsync(Path.Combine(_testCaseDir, "NoDiagnostic", $"{testCase}.al"))
+            .ConfigureAwait(false);
+
+        var fixture = RoslynFixtureFactory.Create<Rule0059SingleQuoteEscaping>();
+        fixture.NoDiagnosticAtMarker(code, DiagnosticDescriptors.Rule0059SingleQuoteEscapingIssueDetected.Id);
+    }
+}
