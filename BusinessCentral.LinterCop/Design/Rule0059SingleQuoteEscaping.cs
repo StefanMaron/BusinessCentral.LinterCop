70fa66a646e9fab15d7b3843f942ae9629be658e
Arthur van de Vondervoort <44637996+Arthurvdv@users.noreply.github.com>
2024-12-05 19:47:21 +0100

    Investigate exception (#823)

--- BusinessCentral.LinterCop/Design/Rule0059SingleQuoteEscaping.cs
+++ BusinessCentral.LinterCop/Design/Rule0059SingleQuoteEscaping.cs
@@ -9,7 +9,7 @@ namespace BusinessCentral.LinterCop.Design
     [DiagnosticAnalyzer]
     public class Rule0059SingleQuoteEscaping : DiagnosticAnalyzer
     {
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create<DiagnosticDescriptor>(DiagnosticDescriptors.Rule0050OperatorAndPlaceholderInFilterExpression, DiagnosticDescriptors.Rule0059SingleQuoteEscapingIssueDetected);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create<DiagnosticDescriptor>(DiagnosticDescriptors.Rule0050OperatorAndPlaceholderInFilterExpression, DiagnosticDescriptors.Rule0059SingleQuoteEscapingIssueDetected, DiagnosticDescriptors.Rule0000ErrorInRule);
 
         private static readonly string InvalidUnaryEqualsFilter = "'<>'''";
 
@@ -18,39 +18,47 @@ namespace BusinessCentral.LinterCop.Design
 
         private void AnalyzeCalcFormula(SymbolAnalysisContext ctx)
         {
-            if (ctx.IsObsoletePendingOrRemoved())
-                return;
-
-            SyntaxNode? syntaxNode = ctx.Symbol.DeclaringSyntaxReference?.GetSyntax(ctx.CancellationToken);
-            if (syntaxNode == null)
-                return;
-
-            if (syntaxNode is not FieldSyntax fieldSyntax)
-                return;
-
-            // Retrieve the 'CalcFormula' property from the field's property list
-            var calcFormulaPropertySyntax = fieldSyntax.PropertyList?.Properties
-                .OfType<PropertySyntax>()
-                .Select(p => p.Value)
-                .OfType<CalculationFormulaPropertyValueSyntax>()
-                .FirstOrDefault();
-
-            if (calcFormulaPropertySyntax is null)
-                return;
-
-            // Retrieve the filter expression from the 'Where' expression of the CalcFormula
-            var filterExpressions = calcFormulaPropertySyntax.WhereExpression.Filter.Conditions
-                .OfType<FilterExpressionSyntax>()
-                .Where(c => c.Filter.Kind == SyntaxKind.UnaryEqualsFilterExpression)
-                .Select(c => c.Filter);
-
-            if (filterExpressions is null)
-                return;
-
-            foreach (var filter in filterExpressions)
+            // Investigate https://github.com/StefanMaron/BusinessCentral.LinterCop/issues/822
+            try
             {
-                if (filter.ToString().Equals(InvalidUnaryEqualsFilter))
-                    ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0059SingleQuoteEscapingIssueDetected, filter.GetLocation()));
+                if (ctx.IsObsoletePendingOrRemoved())
+                    return;
+
+                SyntaxNode? syntaxNode = ctx.Symbol.DeclaringSyntaxReference?.GetSyntax(ctx.CancellationToken);
+                if (syntaxNode == null)
+                    return;
+
+                if (syntaxNode is not FieldSyntax fieldSyntax)
+                    return;
+
+                // Retrieve the 'CalcFormula' property from the field's property list
+                var calcFormulaPropertySyntax = fieldSyntax.PropertyList?.Properties
+                    .OfType<PropertySyntax>()
+                    .Select(p => p.Value)
+                    .OfType<CalculationFormulaPropertyValueSyntax>()
+                    .FirstOrDefault();
+
+                if (calcFormulaPropertySyntax is null)
+                    return;
+
+                // Retrieve the filter expression from the 'Where' expression of the CalcFormula
+                var filterExpressions = calcFormulaPropertySyntax.WhereExpression.Filter.Conditions
+                    .OfType<FilterExpressionSyntax>()
+                    .Where(c => c.Filter.Kind == SyntaxKind.UnaryEqualsFilterExpression)
+                    .Select(c => c.Filter);
+
+                if (filterExpressions is null)
+                    return;
+
+                foreach (var filter in filterExpressions)
+                {
+                    if (filter.ToString().Equals(InvalidUnaryEqualsFilter))
+                        ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0059SingleQuoteEscapingIssueDetected, filter.GetLocation()));
+                }
+            }
+            catch (NullReferenceException)
+            {
+                ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0000ErrorInRule, ctx.Symbol.GetLocation(), new Object[] { "Rule0029", "NullReferenceException", "" }));
             }
         }
     }
