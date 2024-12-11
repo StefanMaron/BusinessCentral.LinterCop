ff48c3f0c32151570cd43a39d7a420f28ef72d5c
Tine Staric <tine.staric@companial.com>
2024-12-11 09:09:45 +0200

    Add three rules WIP

---
+++ BusinessCentral.LinterCop/Design/Rule0077MissingParenthesis.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Dynamics.Nav.CodeAnalysis;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Syntax;
+using System.Collections.Immutable;
+using BusinessCentral.LinterCop.AnalysisContextExtension;
+
+namespace BusinessCentral.LinterCop.Design;
+
+[DiagnosticAnalyzer]
+public class Rule0077MissingParenthesis : DiagnosticAnalyzer
+{
+    private static readonly ImmutableHashSet<string> MethodsRequiringParenthesis = ImmutableHashSet.Create(
+        "Count",
+        "IsEmpty"
+    );
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create<DiagnosticDescriptor>(DiagnosticDescriptors.Rule0077MissingParenthesis);
+
+    public override void Initialize(AnalysisContext context) => 
+        context.RegisterOperationAction(AnalyzeParenthesis, OperationKind.InvocationExpression);
+
+    private void AnalyzeParenthesis(OperationAnalysisContext ctx)
+    {
+        if (ctx.IsObsoletePendingOrRemoved())
+            return;
+
+        if (ctx.Operation is not IInvocationExpression { Arguments.Length: 0 } operation ||
+            operation.TargetMethod is not IMethodSymbol { MethodKind: MethodKind.BuiltInMethod } method)
+            return;
+
+        if (!operation.Syntax.GetLastToken().IsKind(SyntaxKind.CloseParenToken) &&
+            MethodsRequiringParenthesis.Contains(method.Name))
+        {
+            ctx.ReportDiagnostic(Diagnostic.Create(
+                DiagnosticDescriptors.Rule0077MissingParenthesis,
+                ctx.Operation.Syntax.GetLocation(),
+                method.Name));
+        }
+    }
+
+    private static class DiagnosticDescriptors
+    {
+        public static readonly DiagnosticDescriptor Rule0077MissingParenthesis = new(
+            id: LinterCopAnalyzers.AnalyzerPrefix + "0077",
+            title: LinterCopAnalyzers.GetLocalizableString("Rule0077MissingParenthesisTitle"),
+            messageFormat: LinterCopAnalyzers.GetLocalizableString("Rule0077MissingParenthesisFormat"),
+            category: "Design",
+            defaultSeverity: DiagnosticSeverity.Warning, isEnabledByDefault: true,
+            description: LinterCopAnalyzers.GetLocalizableString("Rule0077MissingParenthesisDescription"),
+            helpLinkUri: "https://github.com/StefanMaron/BusinessCentral.LinterCop/wiki/LC0077");
+    }    
+}
---
+++ BusinessCentral.LinterCop/Design/Rule0078TempRecRunTrigger.cs
@@ -0,0 +1,65 @@
+﻿using BusinessCentral.LinterCop.AnalysisContextExtension;
+using CompanialCopAnalyzer.Design.Helper;
+using Microsoft.Dynamics.Nav.CodeAnalysis;
+using Microsoft.Dynamics.Nav.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace BusinessCentral.LinterCop.Design;
+
+[DiagnosticAnalyzer]
+public class Rule0078TemporaryRecordsShouldNotTriggerTableTriggers : DiagnosticAnalyzer
+{
+    private static readonly HashSet<string> methodsToCheck = new() { "Insert", "Modify", "Delete", "DeleteAll", "Validate", "ModifyAll" };
+    private static readonly string validateMethod = "Validate";
+    private static readonly string modifyAllMethod = "ModifyAll";
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(DiagnosticDescriptors.Rule0078TemporaryRecordsShouldNotTriggerTableTriggers);
+
+    public override void Initialize(AnalysisContext context) =>
+        context.RegisterOperationAction(AnalyzeTemporaryRecords, OperationKind.InvocationExpression);
+
+    private void AnalyzeTemporaryRecords(OperationAnalysisContext ctx)
+    {
+        if (ctx.IsObsoletePendingOrRemoved())
+            return;
+
+        var invocationExpression = (IInvocationExpression)ctx.Operation;
+        if (!methodsToCheck.Contains(invocationExpression.TargetMethod.Name))
+            return;
+
+        if (!(invocationExpression.Instance?.Type is IRecordTypeSymbol record) || !record.Temporary)
+            return;
+
+        bool isExecutingTriggersOrValidation = invocationExpression.TargetMethod.Name switch
+        {
+            validateMethod => true,
+            modifyAllMethod => invocationExpression.Arguments.Length == 3 &&
+                               IsRunTriggerEnabled(invocationExpression.Arguments[2]),
+            _ => invocationExpression.Arguments.Length == 1 &&
+                 IsRunTriggerEnabled(invocationExpression.Arguments[0])
+        };
+
+        if (isExecutingTriggersOrValidation)
+        {
+            ctx.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.Rule0078TemporaryRecordsShouldNotTriggerTableTriggers,
+                ctx.Operation.Syntax.GetLocation()));
+        }
+    }
+
+    private static bool IsRunTriggerEnabled(IArgument argument) =>
+        argument.Value.ConstantValue.HasValue &&
+        argument.Value.ConstantValue.Value is bool isEnabled &&
+        isEnabled;
+
+    public static class DiagnosticDescriptors
+    {
+        public static readonly DiagnosticDescriptor Rule0078TemporaryRecordsShouldNotTriggerTableTriggers = new(
+            id: LinterCopAnalyzers.AnalyzerPrefix + "0078",
+            title: LinterCopAnalyzers.GetLocalizableString("Rule0078TemporaryRecordsShouldNotTriggerTableTriggersTitle"),
+            messageFormat: LinterCopAnalyzers.GetLocalizableString("Rule0078TemporaryRecordsShouldNotTriggerTableTriggersFormat"),
+            category: "Design",
+            defaultSeverity: DiagnosticSeverity.Warning, isEnabledByDefault: true,
+            description: LinterCopAnalyzers.GetLocalizableString("Rule0078TemporaryRecordsShouldNotTriggerTableTriggersDescription"),
+            helpLinkUri: "https://github.com/StefanMaron/BusinessCentral.LinterCop/wiki/LC0078");
+    }
+}
