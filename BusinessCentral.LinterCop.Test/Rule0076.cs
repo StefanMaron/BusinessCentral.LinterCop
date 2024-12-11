fef364a76fb3d395035ddbf44f11140d0541cdfa
Tine Staric <tine.staric@companial.com>
2024-12-11 13:22:31 +0200

    Add Tests for Table Relation

---
+++ BusinessCentral.LinterCop.Test/Rule0076.cs
@@ -0,0 +1,39 @@
+namespace BusinessCentral.LinterCop.Test;
+
+public class Rule0076
+{
+    private string _testCaseDir = "";
+
+    [SetUp]
+    public void Setup()
+    {
+        _testCaseDir = Path.Combine(Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.FullName,
+            "TestCases", "Rule0076");
+    }
+
+    [Test]
+    [TestCase("TableRelationLonger")]
+    [TestCase("TableExtRelationShorter")]
+    public async Task HasDiagnostic(string testCase)
+    {
+        var code = await File.ReadAllTextAsync(Path.Combine(_testCaseDir, "HasDiagnostic", $"{testCase}.al"))
+            .ConfigureAwait(false);
+
+        var fixture = RoslynFixtureFactory.Create<Rule0076TableRelationTooLong>();
+        fixture.HasDiagnostic(code, Rule0076TableRelationTooLong.DiagnosticDescriptors.Rule0076TableRelationTooLong.Id);
+    }
+
+    [Test]
+    [TestCase("TableRelationEqual")]
+    [TestCase("TableRelationShorter")]
+    [TestCase("TableExtRelationEqual")]
+    [TestCase("TableExtRelationLonger")]
+    public async Task NoDiagnostic(string testCase)
+    {
+        var code = await File.ReadAllTextAsync(Path.Combine(_testCaseDir, "NoDiagnostic", $"{testCase}.al"))
+            .ConfigureAwait(false);
+
+        var fixture = RoslynFixtureFactory.Create<Rule0076TableRelationTooLong>();
+        fixture.NoDiagnosticAtMarker(code, Rule0076TableRelationTooLong.DiagnosticDescriptors.Rule0076TableRelationTooLong.Id);
+    }
+}
---
+++ BusinessCentral.LinterCop.Test/TestCases/Rule0076/NoDiagnostic/TableExtRelationShorter.al
@@ -0,0 +1,33 @@
+table 50108 MyTable
+{
+    fields
+    {
+        field(1; [|MyText|]; Text[200])
+        {
+            TableRelation = MyTable.MyTextExt;
+        }
+
+        field(2; [|MyCode|]; Text[200])
+        {
+            TableRelation = if (MyCode = const('const')) MyTable.MyCodeExt 
+            else 
+            MyTable.MyTextExt;
+        }
+    }
+
+    keys
+    {
+        key(PK; MyText) { Clustered = true; }
+    }
+}
+
+
+tableextension 50108 MyExtension extends MyTable
+{
+    fields
+    {
+        field(3; MyTextExt; Text[100]) { }
+
+        field(4; MyCodeExt; Text[100]) { }
+    }
+}
