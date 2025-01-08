09e8d530500dc8d2c9088e01900ea3c3cc4d92c7
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2025-01-08 10:33:56 +0100

    Fix wrong class

--- BusinessCentral.LinterCop.Test/Rule0023.cs
+++ BusinessCentral.LinterCop.Test/Rule0023.cs
@@ -19,7 +19,7 @@ public class Rule0023
         var code = await File.ReadAllTextAsync(Path.Combine(_testCaseDir, "HasDiagnostic", $"{testCase}.al"))
             .ConfigureAwait(false);
 
-        var fixture = RoslynFixtureFactory.Create<BuiltInMethodImplementThroughCodeunit>();
+        var fixture = RoslynFixtureFactory.Create<Rule0023AlwaysSpecifyFieldgroups>();
         fixture.HasDiagnostic(code, DiagnosticDescriptors.Rule0023AlwaysSpecifyFieldgroups.Id);
     }
 
@@ -30,7 +30,7 @@ public class Rule0023
         var code = await File.ReadAllTextAsync(Path.Combine(_testCaseDir, "NoDiagnostic", $"{testCase}.al"))
             .ConfigureAwait(false);
 
-        var fixture = RoslynFixtureFactory.Create<BuiltInMethodImplementThroughCodeunit>();
+        var fixture = RoslynFixtureFactory.Create<Rule0023AlwaysSpecifyFieldgroups>();
         fixture.NoDiagnosticAtMarker(code, DiagnosticDescriptors.Rule0023AlwaysSpecifyFieldgroups.Id);
     }
 }
