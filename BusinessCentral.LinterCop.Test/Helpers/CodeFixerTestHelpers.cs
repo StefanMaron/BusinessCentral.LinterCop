49c36c62475f74ca6676d6169e18c840b1bfd515
dannoe <github@dannoe.de>
2025-03-22 22:59:26 +0100

    Added test for codefixer

---
+++ BusinessCentral.LinterCop.Test/Helpers/CodeFixerTestHelpers.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessCentral.LinterCop.Test.Helpers;
+
+internal static class CodeFixerTestHelpers
+{
+    private static readonly string BasePath = Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.FullName;
+ 
+    public static async Task<string> GetCodeFixerTestCode(string className, params string[] folders)
+    {
+        return await File.ReadAllTextAsync(Path.Combine([BasePath, "CodeFixerTests", className, .. folders])).ConfigureAwait(false);
+    }
+}
