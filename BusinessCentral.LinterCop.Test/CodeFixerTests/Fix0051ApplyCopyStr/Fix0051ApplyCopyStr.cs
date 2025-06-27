40693cf3ec61c358ec237ad7e6b1f280d23601f9
Arthur van de Vondervoort <arthur.vandevondervoort@vanroey.be>
2025-06-27 14:59:06 +0200

    Resolve build errros for AL 12.0

--- BusinessCentral.LinterCop.Test/CodeFixerTests/Fix0051ApplyCopyStr/Fix0051ApplyCopyStr.cs
+++ BusinessCentral.LinterCop.Test/CodeFixerTests/Fix0051ApplyCopyStr/Fix0051ApplyCopyStr.cs
@@ -4,9 +4,8 @@ using BusinessCentral.LinterCop.Test.Helpers;
 namespace BusinessCentral.LinterCop.Test.CodeFixerTests.Fix0051ApplyCopyStr;
 
 // Somehow AL version 12.0 throws an error 
-// Fix0051.cs(20,44): error CS0246: The type or namespace name 'Rule0051PossibleOverflowAssigning' could not be found (are you missing a using directive or an assembly reference?) 
-#if !LessThenFall2023
-
+// error CS0246: The type or namespace name 'Rule0051PossibleOverflowAssigning' could not be found (are you missing a using directive or an assembly reference?) 
+#if !LessThenFall2023RV1
 internal class Fix0051ApplyCopyStr
 {
     [Test]
