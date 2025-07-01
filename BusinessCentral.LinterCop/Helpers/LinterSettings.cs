ef298c7f2c8e997182ffb6e56559c38a7c1575da
Arthur van de Vondervoort <44637996+Arthurvdv@users.noreply.github.com>
2025-07-01 10:43:05 +0200

    Improve handling LinterCop.json (#1105)
    * Improve handling retrieving LinterCop.json
    
    * Set defaulft is no settingsFile exists
    
    * Remove unnecessary else
    
    * Check for empty WorkingDir

--- BusinessCentral.LinterCop/Helpers/LinterSettings.cs
+++ BusinessCentral.LinterCop/Helpers/LinterSettings.cs
@@ -5,10 +5,11 @@ namespace BusinessCentral.LinterCop.Helpers
 {
     class LinterSettings
     {
+        private const string settingsFileName = "LinterCop.json";
         public int cyclomaticComplexityThreshold = 8;
         public int maintainabilityIndexThreshold = 20;
         public int cognitiveComplexityThreshold = 15;
-        public String[] languagesToTranslate = null;
+        public string[] languagesToTranslate = null;
         public bool enableRule0011ForTableFields = false;
         public bool enableRule0016ForApiObjects = false;
         public string WorkingDir = "";
@@ -18,34 +19,33 @@ namespace BusinessCentral.LinterCop.Helpers
         {
             if (instance is null || instance.WorkingDir != WorkingDir)
             {
-                try
+                instance = new LinterSettings();
+
+                if (string.IsNullOrEmpty(WorkingDir))
+                    return;
+
+                string settingsPath = Path.Combine(WorkingDir, settingsFileName);
+
+                // If the settings file is not found in the working directory, look in the location of the LinterCop file itself
+                if (!File.Exists(settingsPath))
+                    settingsPath = Path.Combine(Path.GetDirectoryName(typeof(LinterSettings).Assembly.Location), settingsFileName);
+
+                if (File.Exists(settingsPath))
                 {
-                    StreamReader r = null;
-                    if (File.Exists(Path.Combine(WorkingDir, "LinterCop.json")))
-                    {
-                        r = File.OpenText(Path.Combine(WorkingDir, "LinterCop.json"));
-                    }
-                    else
-                    {
-                        r = File.OpenText(Path.Combine(Path.GetDirectoryName(typeof(LinterSettings).Assembly.Location)!, "LinterCop.json")); 
-                    }
-                    string json = r.ReadToEnd();
-                    r.Close();
-                    instance = new LinterSettings();
+                    using StreamReader reader = File.OpenText(settingsPath);
+                    string json = reader.ReadToEnd();
 
                     InternalLinterSettings internalInstance = JsonConvert.DeserializeObject<InternalLinterSettings>(json);
+
                     instance.cyclomaticComplexityThreshold = internalInstance.cyclomaticComplexityThreshold ?? instance.cyclomaticComplexityThreshold;
                     instance.maintainabilityIndexThreshold = internalInstance.maintainabilityIndexThreshold ?? instance.maintainabilityIndexThreshold;
                     instance.cognitiveComplexityThreshold = internalInstance.cognitiveComplexityThreshold ?? instance.cognitiveComplexityThreshold;
                     instance.languagesToTranslate = internalInstance.languagesToTranslate ?? instance.languagesToTranslate;
                     instance.enableRule0011ForTableFields = internalInstance.enableRule0011ForTableFields;
                     instance.enableRule0016ForApiObjects = internalInstance.enableRule0016ForApiObjects;
-                    instance.WorkingDir = WorkingDir;
                 }
-                catch
-                {
-                    instance = new LinterSettings();
-                }
+
+                instance.WorkingDir = WorkingDir;
             }
         }
     }
@@ -55,7 +55,7 @@ namespace BusinessCentral.LinterCop.Helpers
         public int? cyclomaticComplexityThreshold;
         public int? maintainabilityIndexThreshold;
         public int? cognitiveComplexityThreshold;
-        public String[] languagesToTranslate;
+        public string[] languagesToTranslate;
         public bool enableRule0011ForTableFields = false;
         public bool enableRule0016ForApiObjects = false;
     }
